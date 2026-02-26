b9c8f90214793e0a0240974cd8edc8dd24790f35
SpicyDarkFox <foxyflutter6@gmail.com>
2026-02-26 13:16:34 +0300

    некоторые исправления

--- Content.Server/Procedural/DungeonJob/DungeonJob.Ore.cs
+++ Content.Server/Procedural/DungeonJob/DungeonJob.Ore.cs
@@ -141,10 +141,10 @@ public sealed partial class DungeonJob
                     }
                 }
 
-                if (groupSize > 0)
-                {
-                    _sawmill.Warning($"Found remaining group size for ore veins of {gen.Replacement ?? "null"}!");
-                }
+                // if (groupSize > 0)   //Pulsar edit: оно слишком много спамит
+                // {
+                //     _sawmill.Warning($"Found remaining group size for ore veins of {gen.Replacement ?? "null"}!");
+                // }
             }
         }
     }
